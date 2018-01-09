49f5bdad478984599dd9cb56b2140676fe77fcf7
vadim <gabderahmanov99@gmail.com>
2018-01-09 23:17:06 +0400

    db dump #3

--- src/main/java/com/brobooklibrary/starter/Starter.java
+++ src/main/java/com/brobooklibrary/starter/Starter.java
@@ -4,12 +4,10 @@ public class Starter {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
-
 	}
-	
-		public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+
+    public void my() {
+        System.out.println("Hello World");
+    }
 
 }
